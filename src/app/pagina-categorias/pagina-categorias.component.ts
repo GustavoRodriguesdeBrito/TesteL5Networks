3552cc38d622ff6c863ffa7e42b5c7f267b5533c
Gustavo Rodrigues de Brito <grb122000@gmail.com>
2024-06-16 00:48:08 -0300

    Implementar sidebar

---
+++ src/app/pagina-categorias/pagina-categorias.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina-categorias',
+  standalone: true,
+  imports: [],
+  templateUrl: './pagina-categorias.component.html',
+  styleUrl: './pagina-categorias.component.css'
+})
+export class PaginaCategoriasComponent {
+
+}
---
+++ src/app/pagina-produtos/pagina-produtos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina-produtos',
+  standalone: true,
+  imports: [],
+  templateUrl: './pagina-produtos.component.html',
+  styleUrl: './pagina-produtos.component.css'
+})
+export class PaginaProdutosComponent {
+
+}
