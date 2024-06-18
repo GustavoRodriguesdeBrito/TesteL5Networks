697ca13616c4efff445f4f1c955d65ab9e7ba204
Gustavo Rodrigues de Brito <grb122000@gmail.com>
2024-06-18 09:41:18 -0300

    Habilitar listagem de produtos

--- src/app/pagina-produtos/pagina-produtos.component.ts
+++ src/app/pagina-produtos/pagina-produtos.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Produto } from '../interfaces/produto';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-pagina-produtos',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './pagina-produtos.component.html',
-  styleUrl: './pagina-produtos.component.css'
+  styleUrl: './pagina-produtos.component.css',
 })
-export class PaginaProdutosComponent {
+export class PaginaProdutosComponent implements OnInit {
+  listaProdutos: Produto[] = [];
 
+  constructor(private api: ApiService) {}
+  ngOnInit() {
+    console.log('prod-init triggered');
+    this.api.getProdutos().subscribe((produtos: Produto[]) => {
+      this.listaProdutos = produtos;
+    });
+  }
 }
